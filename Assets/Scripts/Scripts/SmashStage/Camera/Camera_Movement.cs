361d9b5e9e5ebd5a4826e1b41d6a3b04c77ac10c
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2019-10-22 18:32:19 +0200

    First Init!

---
+++ Assets/Scripts/Scripts/SmashStage/Camera/Camera_Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Camera_Movement : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Vector3 offset;
+    public float smoothTime = 0.5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float limitZoom = 50f;
+
+    private Bounds bounds;
+    private Vector3 velocity;
+    private Camera cam;
+
+    public void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+        GetBounds();
+        Move();
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, Mathf.Max(bounds.size.x, bounds.size.y) / limitZoom);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    void GetBounds()
+    {
+        bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+    }
+
+
+    Vector3 GetCenterPoint()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        return bounds.center;
+    }
+}
---
+++ Assets/Scripts/Scripts/SmashStage/Movement/Player_movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public bool useArrows;
+
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 0.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input
+        if (useArrows)
+        {
+            moveDirection = new Vector3(Input.GetAxis("HorizontalArrow"), Input.GetAxis("VerticalArrow"), 0.0f);
+        }
+
+        if (!useArrows)
+        {
+            moveDirection = new Vector3(Input.GetAxis("HorizontalWASD"), Input.GetAxis("VerticalWASD"), 0.0f);
+        }
+
+        moveDirection *= speed;
+
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
