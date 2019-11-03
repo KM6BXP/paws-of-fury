d5e5265da880d9de80fbaad616ba3c635e154460
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2019-11-03 17:41:03 +0100

    changed token button to space

--- Assets/Import/Scripts/CursorDetection.cs
+++ Assets/Import/Scripts/CursorDetection.cs
@@ -10,6 +10,8 @@ public class CursorDetection : MonoBehaviour {
     private GraphicRaycaster gr;
     private PointerEventData pointerEventData = new PointerEventData(null);
 
+    private float timer;
+
     public Transform currentCharacter;
 
     public Transform token;
@@ -24,22 +26,25 @@ public class CursorDetection : MonoBehaviour {
     }
 
     void Update () {
-
+        if (timer < .5f)
+            timer += Time.deltaTime;
         //CONFIRM
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Space) && hasToken && timer >= .5f)
         {
             if (currentCharacter != null)
             {
                 TokenFollow(false);
                 SmashCSS.instance.ConfirmCharacter(0, SmashCSS.instance.characters[currentCharacter.GetSiblingIndex()]);
             }
+            timer = 0f;
         }
 
         //CANCEL
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.Space) && !hasToken && timer >= .5f)
         {
             SmashCSS.instance.confirmedCharacter = null;
             TokenFollow(true);
+            timer = 0f;
         }
 
         if (hasToken)
@@ -56,6 +61,14 @@ public class CursorDetection : MonoBehaviour {
             if (results.Count > 0)
             {
                 Transform raycastCharacter = results[0].gameObject.transform;
+
+                if (results[0].gameObject.GetComponent<Button>() != null && Input.GetKeyDown(KeyCode.Space) && timer >= .5f)
+                {
+                    Debug.Log("we got quit");
+                    raycastCharacter.GetComponent<Button>().onClick.Invoke();
+                    timer = 0f;
+                    return;
+                }
 
                 if (raycastCharacter != currentCharacter)
                 {
