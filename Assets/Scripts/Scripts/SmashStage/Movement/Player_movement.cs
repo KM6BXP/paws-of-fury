47ae61fed204b9462a759752f874e7f8c8a1cf0d
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2019-11-08 21:33:26 +0100

    fine tuned default controls

--- Assets/Scripts/Scripts/SmashStage/Movement/Player_movement.cs
+++ Assets/Scripts/Scripts/SmashStage/Movement/Player_movement.cs
@@ -11,23 +11,23 @@ public class Player_movement : MonoBehaviour
     //set per character and per level information
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 0.0f;
+    public float gravity = 9.81f;
 
-    public float JumpCooldown = 1f;
-    private float timer;
+    public float weight = 20f;
+
+    private bool m_isJumpInUse = false;
 
     private Vector3 moveVelocity = Vector3.zero;
     private int jump = 0;
     void Start()
     {
-        timer = JumpCooldown;
         characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         //Get horizontal input
         if (useArrows)
         {
@@ -45,49 +45,28 @@ public class Player_movement : MonoBehaviour
             moveVelocity.y = 0;
         }
 
-        //double jump here, otherwise the character will jump immediately again
-        //also countdown to prevent jumping twice in 2 frames
-        if(timer <= 0) {
-            if (jump == 1)
-            {
-                jump++;
-                if (useArrows)
-                {
-                    if (Input.GetAxis("VerticalArrow") > 0)
-                    {
-                        moveVelocity.y = jumpSpeed;
-                    }
-                }
-
-                if (!useArrows)
-                {
-                    if (Input.GetAxis("VerticalWASD") > 0)
-                    {
-                        moveVelocity.y = jumpSpeed;
-                    }
-                }
-            }
-        }
-        else
-        {
-            timer -= Time.deltaTime;
-        }
-
         //Jumping
         if ((characterController.collisionFlags & CollisionFlags.Below) != 0)
         {
             //dont want to move down when touching ground
             moveVelocity.y = 0;
             jump = 0;
-
-            
+        }
+        else
+        {
+            // Apply gravity when not on the ground. Gravity is multiplied by deltaTime twice (once here, and once below
+            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+            // as an acceleration (ms^-2)
+            moveVelocity.y -= gravity * Time.deltaTime * weight;
+        }
+        if (!m_isJumpInUse && jump < 2)
+        {
             if (useArrows)
             {
                 if (Input.GetAxis("VerticalArrow") > 0)
                 {
                     moveVelocity.y = jumpSpeed;
                     jump++;
-                    timer = JumpCooldown;
                 }
             }
 
@@ -97,17 +76,16 @@ public class Player_movement : MonoBehaviour
                 {
                     moveVelocity.y = jumpSpeed;
                     jump++;
-                    timer = JumpCooldown;
                 }
             }
+            m_isJumpInUse = true;
         }
-        else
-        {
-            // Apply gravity when not on the ground. Gravity is multiplied by deltaTime twice (once here, and once below
-            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-            // as an acceleration (ms^-2)
-            moveVelocity.y -= gravity * Time.deltaTime;
-        }
+        if (useArrows && Input.GetAxis("VerticalArrow") == 0)
+                m_isJumpInUse = false;
+
+        if (!useArrows && Input.GetAxis("VerticalWASD") == 0)
+                m_isJumpInUse = false;
+
 
         // Move the controller
         Vector3 moveDirection = moveVelocity * Time.deltaTime;
