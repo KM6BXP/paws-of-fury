9f10c6cf9ff3fd46e1c8e641df43d34d48e7448b
Keene <54332999+notkeeneonthis@users.noreply.github.com>
2019-11-08 23:13:43 +0000

    Optimized cursor boundary code
    Now setting worldSize to Start() function, so it does not refresh every frame. Since resolution changes cannot be made on this screen, this should have no side effects.

--- Assets/Scripts/Scripts/CharacterSelection/CursorMovement.cs
+++ Assets/Scripts/Scripts/CharacterSelection/CursorMovement.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class CursorMovement : MonoBehaviour {
 
     public float speed;
+    private Vector3 worldSize;
 
-	void Update () {
+    void Start()
+    {
+        worldSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+
+    void Update () {
 
         float x = Input.GetAxis("HorizontalWASD");
         float y = Input.GetAxis("VerticalWASD");
 
         transform.position += new Vector3(x, y, 0) * Time.deltaTime * speed;
-
-        Vector3 worldSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -worldSize.x, worldSize.x),
             Mathf.Clamp(transform.position.y, -worldSize.y, worldSize.y),
