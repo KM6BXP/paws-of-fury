278fd17e6e4c78f8c73543b34afa33475f062078
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2019-11-13 17:28:28 +0100

    Fixed jumping for the last time hopefully

--- Assets/Scripts/SmashStage/Movement/Player_movement.cs
+++ Assets/Scripts/SmashStage/Movement/Player_movement.cs
@@ -8,17 +8,20 @@ public class Player_movement : MonoBehaviour
 
     CharacterController characterController;
 
-    //set per character and per level information
+    //Set per character and per level information
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 9.81f;
 
     public float weight = 20f;
 
-    private bool m_isJumpInUse = false;
-
-    public Vector3 moveVelocity = Vector3.zero;
+    //Jumping
+    private float coyoteTime = 0;
+    private const float const_maxCoyoteTime = 0.1f;
     private int jump = 0;
+    private bool m_isJumpInUse = false;
+    public Vector3 moveVelocity = Vector3.zero;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -39,27 +42,10 @@ public class Player_movement : MonoBehaviour
             moveVelocity.x = Input.GetAxis("HorizontalWASD") * speed;
         }
 
-        //make sure object doesn't stick to a ceiling
+        //Make sure object doesn't stick to a ceiling
         if ((characterController.collisionFlags & CollisionFlags.Above) != 0 && moveVelocity.y > 0)
         {
             moveVelocity.y = 0;
-        }
-
-        //Jumping
-        if (characterController.isGrounded)
-        {
-            //dont want to move down when touching ground
-            moveVelocity.y = 0;
-            jump = 0;
-        }
-        else
-        {
-            // Apply gravity when not on the ground. Gravity is multiplied by deltaTime twice (once here, and once below
-            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-            // as an acceleration (ms^-2)
-            moveVelocity.y -= gravity * Time.deltaTime * weight;
-            if (jump < 1 && !characterController.isGrounded)
-                jump = 1;
         }
 
         if (!m_isJumpInUse && jump < 2)
@@ -83,11 +69,29 @@ public class Player_movement : MonoBehaviour
             }
             m_isJumpInUse = true;
         }
+
         if (useArrows && Input.GetAxis("VerticalArrow") == 0)
             m_isJumpInUse = false;
 
         if (!useArrows && Input.GetAxis("VerticalWASD") == 0)
             m_isJumpInUse = false;
+
+        //Jumping
+        if (isGrounded())
+        {
+            //Dont want to move down when touching ground
+            moveVelocity.y = -gravity * Time.deltaTime * weight; ;
+            jump = 0;
+        }
+        else
+        {
+            // Apply gravity when not on the ground. Gravity is multiplied by deltaTime twice (once here, and once below
+            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+            // as an acceleration (ms^-2)
+            moveVelocity.y -= gravity * Time.deltaTime * weight;
+            if (jump < 1)
+                jump = 1;
+        }
 
 
         // Move the controller
@@ -96,5 +100,21 @@ public class Player_movement : MonoBehaviour
 
         //Make sure to never ever move on the z axis  or bad stuff will happen
         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+        //This is to smooth isGrounded so it isn't freaking out
+        if (characterController.isGrounded)
+        {
+            coyoteTime = 0;
+        }
+        else if(coyoteTime <= const_maxCoyoteTime)
+        {
+            coyoteTime += Time.deltaTime;
+        }
+    }
+
+    //Return a smoothed isGrounded and looks at velocity.y
+    public bool isGrounded()
+    {
+        return coyoteTime <= const_maxCoyoteTime && moveVelocity.y <= 0;
     }
 }
