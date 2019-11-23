adf25ae78e602e38c6fbae2b28040fc8be9eecc0
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2019-11-23 20:04:49 +0100

    added second test map

--- Assets/Scripts/Main_Menu/Copyright.cs
+++ Assets/Scripts/Main_Menu/Copyright.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Copyright : MonoBehaviour
 {
-
-    [TextArea(15,20), Tooltip("use %version% for version location")]
+    public Button button;
+    [TextArea(15, 20), Tooltip("use %version% for version location")]
     public string copyrights;
     public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
         text.text = copyrights.Replace("%version%", Application.version);
+    }
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Buttonpress();
+        }
+    }
+    public void Buttonpress()
+    {
+        button.onClick.Invoke();
     }
 }
